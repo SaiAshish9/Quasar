f9d7f9710c495e24193f9df6ed2f630baf5161b5
SaiAshish9 <saiashish3760@gmail.com>
2020-10-26 01:28:35 +0530

    camera page updated

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,72 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-white text-grey-10" bordered>
+      <q-toolbar class="constrain">
+        <q-btn
+          class="large-screen-only q-mr-sm"
+          to="/camera"
+          dense
+          flat
+          round
+          size="18px"
+          icon="eva-camera-outline"
+        />
+        <q-separator vertical spaced class="large-screen-only" />
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          Quasagram
+        </q-toolbar-title>
+        <q-btn
+          class="large-screen-only"
+          to="/"
+          dense
+          flat
+          round
+          size="18px"
+          icon="eva-home-outline"
+        />
+      </q-toolbar>
+    </q-header>
+    <q-footer class=" small-screen-only bg-white" bordered>
+      <q-tabs
+        indicator-color="transparent"
+        class="text-grey-10"
+        active-color="primary"
+      >
+        <!-- ctrl alt shift a -->
+        <q-route-tab to="/" icon="eva-home-outline" />
+        <!-- label="Home" -->
+
+        <q-route-tab to="/camera" icon="eva-camera-outline" />
+      </q-tabs>
+    </q-footer>
+    <q-page-container class="bg-grey-1">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  components: {},
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="sass">
+.q-toolbar
+  @media (min-width:$breakpoint-sm-min)
+      height: 77px
+
+
+.q-toolbar__title
+  font-size: 30px
+  @media (max-width:$breakpoint-xs-max)
+      text-align: center
+
+.q-footer
+  .q-tab__icon
+    font-size: 30px
+</style>
---
+++ src/pages/PageCamera.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-page class="constrain-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn color="grey-10" icon="eva-camera" size="lg" round />
+      <div class="row justify-center q-ma-md">
+        <q-input v-model="post.caption" label="Caption" class="col col-sm-6" dense />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input v-model="post.location" label="Location" class="col col-sm-6" dense>
+          <template v-slot:append>
+            <q-btn round dense flat icon="eva-navigation-2-outline" />
+          </template>
+        </q-input>
+      </div>
+      <div class="row justify-center q-mt-lg">
+        <q-btn
+        color="primary"
+        rounded
+        unelevated
+        label="Post Image"
+        />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+
+import {uid} from "quasar"
+
+export default {
+  name: "PageCamera",
+  data() {
+    return {
+      post: {
+        id : uid(),
+        caption:'',
+        location: '',
+        photo: null,
+        date:Date.now(),
+      }
+    };
+  }
+};
+</script>
+
+<style lang="sass">
+.camera-frame
+    border: 2px solid $grey-10
+    border-radius: 10px
+</style>
