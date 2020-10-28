3522853fabf84346afed9c1fe39477277deda623
SaiAshish9 <saiashish3760@gmail.com>
2020-10-28 21:25:35 +0530

    updated camera page

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,48 +1,88 @@
 <template>
   <q-page class="constrain-more q-pa-md">
     <div class="camera-frame q-pa-md">
-      <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <video class="full-width" autoplay ref="video" playsinline />
+      <canvas ref="canvas" class="full-width" height="240" />
+      <!-- <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg" /> -->
     </div>
     <div class="text-center q-pa-md">
-      <q-btn color="grey-10" icon="eva-camera" size="lg" round />
+      <q-btn
+        v-if="hasCameraSupport"
+        @click="captureImage"
+        color="grey-10"
+        icon="eva-camera"
+        size="lg"
+        round
+      />
+      <q-file
+        v-else
+        accept="image/*"
+        v-model="imageUpload"
+        label="Choose an image"
+        outlined
+      >
+        <template v-slot:prepend>
+          <q-icon name="eva-attach-outline" />
+        </template>
+      </q-file>
+
       <div class="row justify-center q-ma-md">
-        <q-input v-model="post.caption" label="Caption" class="col col-sm-6" dense />
+        <q-input
+          v-model="post.caption"
+          label="Caption"
+          class="col col-sm-6"
+          dense
+        />
       </div>
       <div class="row justify-center q-ma-md">
-        <q-input v-model="post.location" label="Location" class="col col-sm-6" dense>
+        <q-input
+          v-model="post.location"
+          label="Location"
+          class="col col-sm-6"
+          dense
+        >
           <template v-slot:append>
             <q-btn round dense flat icon="eva-navigation-2-outline" />
           </template>
         </q-input>
       </div>
       <div class="row justify-center q-mt-lg">
-        <q-btn
-        color="primary"
-        rounded
-        unelevated
-        label="Post Image"
-        />
+        <q-btn color="primary" rounded unelevated label="Post Image" />
       </div>
     </div>
   </q-page>
 </template>
 
 <script>
-
-import {uid} from "quasar"
+import { uid } from "quasar";
+require("md-gum-polyfill");
 
 export default {
   name: "PageCamera",
   data() {
     return {
       post: {
-        id : uid(),
-        caption:'',
-        location: '',
+        id: uid(),
+        caption: "",
+        location: "",
         photo: null,
-        date:Date.now(),
+        date: Date.now()
       }
     };
+  },
+  methods: {
+    initCamera: function() {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true
+        })
+        .then(stream => {
+          this.$refs.video.srcObject = stream;
+        });
+    }
+  },
+  mounted() {
+    this.initCamera();
   }
 };
 </script>
